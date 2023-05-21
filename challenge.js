b2ad4d75c5e4d3f21a288bd42e25c82fd653705e
zaki tayyip <zakidayib1997@gmail.com>
2023-05-21 12:01:06 +0300

    BMI challenge solved

--- challenge.js
+++ challenge.js
@@ -1,2 +1,25 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+let bmiCalc = (weight, height)=>{
+
+        let BMI = (weight)/(height**2);
+
+        return BMI;
+}
+
+
+let mohemedBmi = bmiCalc(83, 1.70);
+let aliBmi = bmiCalc(81, 1.70);
+
+if (mohemedBmi > aliBmi){
+    console.log(`mohamed has higher MBI than ali`)
+}
+else if(aliBmi > mohemedBmi) {
+    console.log(`Ali has higher MBI than Mohamed`)
+}
+
+else {
+    console.log(`they have equal BMI`)
+}
